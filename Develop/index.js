7f645d80b64dcf701463a15ba3fdf21be18bc2a6
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-05-11 13:48:38 -0400

    added questions, and function to write/initilize the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your title?",
+        name: "title",
+    },
+    {
+     type: "input",
+     message: "Instructions to use the applications",
+     name: "applicationInstructions",
+    },
+    {
+        type: "input", 
+        message: "Names of members involved in the project?",
+        name: "credits",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.log(err) : console.log('Successfully created README'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.question(questions)
+    .then(function (responsesArr) {
+        writeToFile("README.md", generateMarkdown (responsesArr)) 
+    })
+
+};
 
 // Function call to initialize app
 init();
